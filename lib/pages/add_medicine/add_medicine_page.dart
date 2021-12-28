7ee09d74dfc06246731491cff884b1528c0e26b4
bora <adbr.dev@gmail.com>
2021-12-28 17:11:08 +0900

    feat: 첫번째 추가창 단순 UI1

--- lib/pages/add_medicine/add_medicine_page.dart
+++ lib/pages/add_medicine/add_medicine_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddMedicinePage extends StatelessWidget {
@@ -6,7 +7,50 @@ class AddMedicinePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: const CloseButton(),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '어떤 약이예요?',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Center(
+              child: CircleAvatar(
+                radius: 40,
+                child: CupertinoButton(
+                  onPressed: () {},
+                  child: const Icon(
+                    CupertinoIcons.photo_camera_solid,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              '약 이름',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            TextFormField(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            textStyle: Theme.of(context).textTheme.subtitle1,
+          ),
+          child: Text('다음'),
+        ),
+      ),
     );
   }
 }
