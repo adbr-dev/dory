0c6011a0532804dc7152334c7093acf5bef938ec
bora <adbr.dev@gmail.com>
2021-12-28 17:40:14 +0900

    feat: 첫번째 추가창 단순 UI2, 위젯 디테일

---
+++ lib/components/dory_constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+/// 20.0
+const double regularSpace = 20.0;
+
+/// 40.0
+const double largeSpace = 40.0;
+
+/// 10.0
+const double smallSpace = 10.0;
+
+/// 48.0
+const double submitButtonHeight = 48.0;
+
+/// `all`
+/// 20.0
+const EdgeInsetsGeometry pagePadding = EdgeInsets.all(20.0);
+
+/// `symmetric`
+/// horizontal: 6
+const EdgeInsetsGeometry textFieldContentPadding =
+    EdgeInsets.symmetric(horizontal: 6);
+
+/// `symmetric`
+/// horizontal: 20,
+/// vertical: 10,
+const EdgeInsetsGeometry submitButtonBoxPadding = EdgeInsets.symmetric(
+  horizontal: 20,
+  vertical: 10,
+);
--- lib/pages/add_medicine/add_medicine_page.dart
+++ lib/pages/add_medicine/add_medicine_page.dart
@@ -1,8 +1,22 @@
+import 'package:dory/components/dory_constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AddMedicinePage extends StatelessWidget {
+class AddMedicinePage extends StatefulWidget {
   const AddMedicinePage({Key? key}) : super(key: key);
+
+  @override
+  State<AddMedicinePage> createState() => _AddMedicinePageState();
+}
+
+class _AddMedicinePageState extends State<AddMedicinePage> {
+  final _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,41 +28,63 @@ class AddMedicinePage extends StatelessWidget {
         onTap: () {
           FocusScope.of(context).unfocus();
         },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '어떤 약이예요?',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Center(
-              child: CircleAvatar(
-                radius: 40,
-                child: CupertinoButton(
-                  onPressed: () {},
-                  child: const Icon(
-                    CupertinoIcons.photo_camera_solid,
-                    size: 30,
-                    color: Colors.white,
+        child: Padding(
+          padding: pagePadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '어떤 약이예요?',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              const SizedBox(height: largeSpace),
+              Center(
+                child: CircleAvatar(
+                  radius: 40,
+                  child: CupertinoButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      CupertinoIcons.photo_camera_solid,
+                      size: 30,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Text(
-              '약 이름',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            TextFormField(),
-          ],
+              const SizedBox(height: largeSpace + regularSpace),
+              Text(
+                '약 이름',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              TextFormField(
+                controller: _nameController,
+                maxLength: 20,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.done,
+                style: Theme.of(context).textTheme.bodyText1,
+                decoration: InputDecoration(
+                  hintText: '복용할 약 이름을 기입해주세요.',
+                  hintStyle: Theme.of(context).textTheme.bodyText2,
+                  contentPadding: textFieldContentPadding,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: SafeArea(
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            textStyle: Theme.of(context).textTheme.subtitle1,
+        child: Padding(
+          padding: submitButtonBoxPadding,
+          child: SizedBox(
+            height: submitButtonHeight,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.subtitle1,
+              ),
+              child: const Text('다음'),
+            ),
           ),
-          child: Text('다음'),
         ),
       ),
     );
